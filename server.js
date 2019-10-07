ec9101800dbe692f763e52d4eec33d1220d5cf3f
Brandy Mello <brandyamello@gmail.com>
2019-10-07 10:35:41 -0600

    Add two GET requests one for all dances and one for dances by id

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.locals.dances = [];
 
-app.locals.pets = [
+app.locals.dances = [
   { id: 'a1', name: 'Salsa', type: 'sexy' },
   { id: 'b2', name: 'Hip Hop', type: 'fun' },
   { id: 'c3', name: 'Waltz', type: 'tricky' }
@@ -18,4 +20,16 @@ app.get('/', (request, response) => {
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+});
+
+app.get('/api/v1/dances', (request, response) => {
+  const dances = app.locals.dances;
+
+  return response.json({ dances });
+});
+
+app.get('/api/v1/dances/:id', (request, response) => {
+  const { id } = request.params;
+  const dance= app.locals.dances.find(dance => dance.id === id)
+  return response.status(200).json(dance);
 });
